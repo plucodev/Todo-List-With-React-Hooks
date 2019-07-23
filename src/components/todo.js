ac2868c30ec31c2d1e2e507044f638bdf1a279b3
plucodev <paololucanodeveloper@gmail.com>
2019-07-22 22:52:45 -0400

    Installed Bootstrap and added some style

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { link } from "fs";
+
 const Todo = props => {
   const [todoName, setTodoName] = useState("");
   const [todoList, setTodoList] = useState([]);
@@ -38,20 +39,52 @@ const Todo = props => {
   };
   return (
     <React.Fragment>
-      <input
-        type="text"
-        placeholder="Todo"
-        onChange={inputChangeHandler}
-        value={todoName}
-      />
-      <button type="submit" onClick={todoAddHandler}>
-        Add
-      </button>
-      <ul>
-        {todoList.map((todo, index) => (
-          <li key={index}>{todo.name}</li>
-        ))}
-      </ul>
+      <div className="container mt-5">
+        <h1>Todo List with React Hooks</h1>
+      </div>
+      <div className="container mt-5">
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            onChange={inputChangeHandler}
+            value={todoName}
+            placeholder="Todo"
+            aria-label="Recipient's username"
+            aria-describedby="button-addon2"
+          />
+          <div className="input-group-append">
+            <button
+              className="btn btn-info"
+              type="button"
+              id="button-addon2"
+              onClick={todoAddHandler}
+            >
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <ul className="list-group">
+          {todoList.map((todo, index) => (
+            <li
+              className="list-group-item d-flex justify-content-between align-items-center"
+              key={index}
+            >
+              <span>{todo.name}</span>
+              <div className=" float-right">
+                <button className="btn">
+                  <i className="fas fa-pencil-alt mr-3" />
+                </button>
+                <button className="btn">
+                  <i className="fas fa-trash-alt" />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </React.Fragment>
   );
 };
